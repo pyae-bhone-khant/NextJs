587ddd95149cac92c3cb94866d10b1c91110d87d
pyae-bhone-khant <pyaebhonen4@gmail.com>
2025-12-03 16:49:45 +0630

    Something add

--- dashboard/src/app/pages/product/Addproducts.tsx
+++ dashboard/src/app/pages/product/Addproducts.tsx
@@ -8,13 +8,14 @@ import {
 import { useState, useEffect } from "react";
 
 import CloseIcon from '@mui/icons-material/Close';
+import TextField from '@mui/material/TextField';
 
 
 export default function AddProduct ({closeEvent  } : {closeEvent : any}) {
     return ( 
         <Box sx={{m : 2}}>
 
-         <Typography id="modal-modal-title" variant="h6" component="h2">
+         <Typography id="modal-modal-title" variant="h6" component="h2" align="center">
            Add Product
           </Typography> 
           <IconButton sx={{position : "absolute" , top : "0" , right : "0"}} 
@@ -23,8 +24,13 @@ export default function AddProduct ({closeEvent  } : {closeEvent : any}) {
            >
             <CloseIcon />
           </IconButton>
-          <Grid container spacing={2}></Grid>
-          
+          <Box height={20} />    
+          <Grid   container spacing={2}>
+            <Grid   sx={{display : "flex" , alignItems : "center"}} item  xs ={12}>
+               <TextField   sx={{mt : 1 , minWidth : "350px" , }} id="outlined-basic" label="Outlined" variant="outlined"  size="small" />
+            </Grid>
+          </Grid>
+           <Box sx={{m : 4}}>   </Box>
         </Box>
     )
 }
